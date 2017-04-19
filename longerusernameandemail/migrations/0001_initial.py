e7d695f32090c834d02e158a97605aa77828bf6a
jeremylongo <jeremy.longo@forcity.io>
2017-04-19 11:41:12 +0200

    Switch from south to Django migration

--- longerusernameandemail/migrations/0001_initial.py
+++ longerusernameandemail/migrations/0001_initial.py
@@ -1,29 +1,49 @@
-# encoding: utf-8
-from south.db import db
-from south.v2 import SchemaMigration
-from django.db import models
-from longerusernameandemail import MAX_USERNAME_LENGTH, MAX_EMAIL_LENGTH, REQUIRE_UNIQUE_EMAIL
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import django.core.validators
+from django.conf import settings
 
 
-class Migration(SchemaMigration):
+class Migration(migrations.Migration):
 
-    def forwards(self, orm):
-        # Changing field 'User.username'
-        db.alter_column('auth_user', 'username', models.CharField(max_length=MAX_USERNAME_LENGTH()))
-        # Increase length of email field to match
-        db.alter_column('auth_user', 'email', models.CharField(max_length=MAX_EMAIL_LENGTH()))
-        # Add an index to make email field unique
-        db.create_index('auth_user', ['email'], unique=REQUIRE_UNIQUE_EMAIL())
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
 
-    def backwards(self, orm):
+    operations = [
+        migrations.AlterField(
+            model_name='user',
+            name='email',
+            field=models.EmailField(help_text='Required, 255 characters or fewer. Only letters, numbers, and @, ., +, -, or _ characters.', unique=True, max_length=255, verbose_name='email address'),
+            preserve_default=True,
+        ),
+        migrations.AlterField(
+            model_name='user',
+            name='username',
+            field=models.CharField(help_text='Required, 255 characters or fewer. Only letters, numbers, and @, ., +, -, or _ characters.', unique=True, max_length=255, verbose_name='username', validators=[django.core.validators.RegexValidator('^[\\w.@+-]+$', 'Enter a valid username.', 'invalid')]),
+            preserve_default=True,
+        ),
+    ]
 
-        # Changing field 'User.username'
-        db.alter_column('auth_user', 'username', models.CharField(max_length=35))
-        db.alter_column('auth_user', 'email', models.CharField(max_length=75))
-        db.delete_index('auth_user', ['email'],)
+    def mutate_state(self, project_state, preserve=True):
+        """
+        This is a workaround that allows to store ``auth``
+        migration outside the directory it should be stored.
+        """
+        app_label = self.app_label
+        self.app_label = 'auth'
+        state = super(Migration, self).mutate_state(project_state)
+        self.app_label = app_label
+        return state
 
-    models = {
-
-    }
-
-    complete_apps = ['django_monkeypatches']
+    def apply(self, project_state, schema_editor, collect_sql=False):
+        """
+        Same workaround as described in ``mutate_state`` method.
+        """
+        app_label = self.app_label
+        self.app_label = 'auth'
+        state = super(Migration, self).apply(project_state, schema_editor, collect_sql)
+        self.app_label = app_label
+        return state
